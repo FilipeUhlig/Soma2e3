e7c303ca734aaacf483ca4531aa8d57df641955b
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-24 10:07:13 -0300

    Add files via upload

---
+++ soma2e3(2).py
@@ -0,0 +1,20 @@
+"""
+Programa: soma 2 e 3
+Descrição: Este programa armazena os números 2 e 3, soma ambos os números e imprime na tela.
+Autor: Filipe
+Data: 02/06/2023
+Versão: 0.0.1
+"""
+
+#Atribuição de variáveis
+x = 2
+y = 3
+
+#Entrada de dados
+
+
+#Processamento de dados
+soma = (x+y)
+
+#Saida de dados
+print(soma)
